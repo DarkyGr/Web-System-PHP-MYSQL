d8636c3cc67bffa9072c9b6c82050ad8c869ecf0
Guillermo Riaño <guilledark17@gmail.com>
2023-02-27 10:25:57 -0600

    ✅ Adding: Page Welcome

---
+++ pages/welcome.php
@@ -0,0 +1,27 @@
+<!-- ============== | head | =================-->
+<?php  include "layouts/head.php";     ?>
+<!--==========================================-->
+
+
+<!-- =========== | contenido | ===============-->
+<div class="app-content content container-fluid">
+    <div class="content-wrapper">
+        <div class="content-header row">
+        </div>
+        <div class="content-body">
+
+        </div>
+    </div>
+</div>
+<!--==========================================-->
+
+<!-- ========= | scripts robust | ============-->
+<?php  include "layouts/main_scripts.php"; ?>
+<!--==========================================-->
+
+<!-- ============= | footer | ================-->
+<?php  include "layouts/footer.php";      ?>
+<!--==========================================-->
+
+
+
