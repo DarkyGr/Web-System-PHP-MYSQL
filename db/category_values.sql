433ccfc8da9e8eac2dd805e6776da43f0d778809
Guillermo Riaño <guilledark17@gmail.com>
2023-02-28 18:20:26 -0600

    📖 DOC: Category values bd

---
+++ db/category_values.sql
@@ -0,0 +1,11 @@
+INSERT INTO category (name_c, description_c) VALUES
+('Breakfast', 'Without description'),
+('Fruits & Vegetables', 'Without description'),
+('Dairy', 'Without description'),
+('Meat, Chicken & Fish', 'Without description'),
+('Health & Beauty', 'Without description'),
+('Cleanliness', 'Without description'),
+('Beer, Wine & Sodas', 'Without description'),
+('Bread & Cake', 'Without description'),
+('Icream', 'Without description'),
+('Pets', 'Without description');
