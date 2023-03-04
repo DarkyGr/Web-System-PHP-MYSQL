7c5bd799c7f0401d694cc6ef40ffd4efb991abac
Guillermo Riaño <guilledark17@gmail.com>
2023-03-03 21:34:49 -0600

    👌 Code review : Folder Modals & Include

--- pages/category.php
+++ pages/category.php
@@ -77,26 +77,8 @@ include "layouts/head.php";     ?>
 </div>
 <!--==========================================-->
 
-<!-- ========= | modal | ============-->
-<div class="modal fade text-xs-left" id="create_category" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-				<h4 class="modal-title" id="myModalLabel1"><i class="icon-road2"></i> New category</h4>
-			</div>
-			<div class="modal-body">
-
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn red btn-outline-danger" data-dismiss="modal">Cancel</button>
-				<button type="button" class="btn green btn-outline-success">Save</button>
-			</div>
-		</div>
-	</div>
-</div>
+<!-- ========= | modals | ============-->
+<?php include 'modals/create/CategoryModal.php' ?>
 <!--==========================================-->
 
 <!-- ========= | scripts robust | ============-->
---
+++ pages/modals/create/CategoryModal.php
@@ -0,0 +1,21 @@
+<!-- ========= | modal | ============-->
+<div class="modal fade text-xs-left" id="create_category" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h4 class="modal-title" id="myModalLabel1"><i class="icon-road2"></i> New category</h4>
+			</div>
+			<div class="modal-body">
+
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn red btn-outline-danger" data-dismiss="modal">Cancel</button>
+				<button type="button" class="btn green btn-outline-success">Save</button>
+			</div>
+		</div>
+	</div>
+</div>
+<!--==========================================-->
