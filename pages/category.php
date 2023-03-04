3f85cb3338b76247da8e8795432aa4617e0a0f59
Guillermo Riaño <guilledark17@gmail.com>
2023-03-03 21:31:58 -0600

    🎨 Improved formatting: Changes ID

--- pages/category.php
+++ pages/category.php
@@ -78,14 +78,14 @@ include "layouts/head.php";     ?>
 <!--==========================================-->
 
 <!-- ========= | modal | ============-->
-<div class="modal fade text-xs-left" id="create_category" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2" aria-hidden="true">
+<div class="modal fade text-xs-left" id="create_category" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
-				<h4 class="modal-title" id="myModalLabel2"><i class="icon-road2"></i> New category</h4>
+				<h4 class="modal-title" id="myModalLabel1"><i class="icon-road2"></i> New category</h4>
 			</div>
 			<div class="modal-body">
 
