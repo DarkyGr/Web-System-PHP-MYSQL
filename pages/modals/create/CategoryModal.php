0b1a0686869e035ea1362ae976aa978fb3212501
Guillermo Riaño <guilledark17@gmail.com>
2023-03-03 21:42:09 -0600

    ✅ Adding Tests: Form Structure (Modal)

--- pages/modals/create/CategoryModal.php
+++ pages/modals/create/CategoryModal.php
@@ -9,7 +9,20 @@
 				<h4 class="modal-title" id="myModalLabel1"><i class="icon-road2"></i> New category</h4>
 			</div>
 			<div class="modal-body">
-
+                <div class="form-group">
+                    <label> Category name: </label>
+                    <div class="position-relative has-icon-left">
+                        <input type="text" placeholder="Type the name of the category" class="form-control" id="category_name" autofocus>
+                        <div class="form-control-position"><i class="icon-bag"></i></div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label> Description </label>
+                    <div class="position-relative has-icon-left">
+                        <textarea type="text" rows="7" placeholder="Type a short description" class="form-control" id="category_description"></textarea>
+                        <div class="form-control-position"><i class="icon-file2"></i></div>
+                    </div>
+                </div>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn red btn-outline-danger" data-dismiss="modal">Cancel</button>
