72d2c58cfddfabb11ab86aff2889a2dc9540d32d
Guillermo Riaño <guilledark17@gmail.com>
2023-03-08 14:47:51 -0600

    📖 DOC: Script Subcategory

--- db/subcategory_values.sql
+++ db/subcategory_values.sql
@@ -12,4 +12,8 @@ INSERT INTO subcategory (id_subcategory, id_category, name_sc) VALUES
 (0, 1, 'Cakes');
 
 SELECT sub.id_subcategory, sub.name_sc, ca.name_c AS category, sub.status_sc FROM category ca 
-INNER JOIN subcategory sub ON ca.id_category = sub.id_subcategory;
+INNER JOIN subcategory sub ON ca.id_category = sub.id_category;
+
+SELECT cat.id_category, cat.name_c FROM category cat
+INNER JOIN subcategory sub ON cat.id_category = sub.id_category WHERE sub.id_subcategory = 19
+UNION SELECT id_category, name_c FROM category WHERE status_c = 1;
